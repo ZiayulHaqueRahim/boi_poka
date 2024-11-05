a3875db1123739915b88ed21bdbbfc76dad8a947
Zia <ziair3812@gmail.com>
2024-11-05 14:38:25 +0600

    second commit

---
+++ src/components/Book/Book.jsx
@@ -0,0 +1,41 @@
+
+const Book = ({ book }) => {
+    const { image, bookName, author, tags, category } = book;
+    return (
+        <div className="card bg-base-100 w-96 shadow-xl p-6 rounded-2xl ">
+            <figure className="bg-gray-400 py-8 rounded-xl">
+                <img
+                    className="h-[165px] bg"
+                    src={image}
+                    alt={bookName} />
+            </figure>
+            <div className="card-body">
+                <div className="flex justify-center gap-2">
+                    {
+                        tags.map(tag => <button className="btn btn-xs 4 text-green-400 px-2 py-1 rounded-sm">{tag}</button>)
+                    }
+                </div>
+                <h2 className="card-title">
+                    {bookName}
+                    <div className="badge badge-secondary">NEW</div>
+                </h2>
+                <p>By:{author}</p>
+                <div className="divider divider-neutral"></div>
+                <div className="card-actions justify-between">
+                    <div className="badge badge-outline">{category}</div>
+                    <div className="badge ">
+                        <div className="rating">
+                            <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
+                            <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" defaultChecked />
+                            <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
+                            <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
+                            <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Book;
---
+++ src/components/BookDetails/BookDetails.jsx
@@ -0,0 +1,12 @@
+
+
+const BookDetails = () => {
+    return (
+        <div>
+            <h2>  Book Details  </h2>
+            
+        </div>
+    );
+};
+
+export default BookDetails;
